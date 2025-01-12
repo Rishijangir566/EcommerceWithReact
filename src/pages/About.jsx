9fb7fd369a9aade6667659ee424f2faa6887da4f
Rishijangir566 <rishijangirjr@gmail.com>
2025-01-12 22:40:26 +0530

    About page cerate

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,31 @@
+import { useContext } from "react"
+import { ecomContext } from "../App"
+import CartQty from "../components/CartQty"
+import { Link } from "react-router-dom"
 
 function About() {
+  const{cart}= useContext(ecomContext)
   return (
-    <div>About</div>
+    <>
+    {
+      cart.length>0 ?<div className="all w-full ">
+        {cart.map((item)=>{
+          return <div className="img  m-8 flex gap-8 items-center justify-evenly" key={item.id}>
+              <img src={item.image} className="h-96 w-96  object-contain" alt="" />
+              <div className="about">
+                <h2 className="  text-2xl  mt-3 font-medium">{item.title}</h2>
+                <h3 className=" text-2xl font-bold text-blue-800">price : {item.price}</h3>
+                <p className=" pr-8 mt-3">{item.description}</p>
+                <div className="PlusMinus my-4">
+                  <CartQty/>
+                </div>
+              </div>
+          </div>
+        })}
+    </div> :<h2> Your Cart Is Empty <Link to="/">Go Shoping </Link></h2>
+    }
+    
+    </>
   )
 }
 
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,19 +1,25 @@
 import { useContext } from "react"
 import { ecomContext } from "../App"
+import CartQty from "../components/CartQty"
+import { Link } from "react-router-dom"
 
 function Blog() {
  const {cart}= useContext(ecomContext)
   return (
     <>
-    <di className="all flex justify-evenly ">
+    {
+      cart.length>0 ?<div className="all flex justify-evenly ">
       <div className="cart  w-[70%]">  
         {cart.map((item)=>{
           return <div className="img h-80 w-full my-8 flex gap-8 items-center justify-evenly" key={item.id}>
               <img src={item.image} className="h-full w-[25%] object-contain" alt="" />
-              <div className="about w-[50%]">
+              <div className="about w-[60%]">
                 <h2 className="  text-2xl  mt-3 font-medium">{item.title}</h2>
                 <h3 className=" text-2xl font-bold text-blue-800">price : {item.price}</h3>
                 <p className=" pr-8 mt-3">{item.description}</p>
+                <div className="PlusMinus my-4">
+                  <CartQty/>
+                </div>
               </div>
           </div>
         })} 
@@ -22,7 +28,9 @@ function Blog() {
         <h2>Total</h2>
         <h3 className="  text-2xl text-start ml-8 mt-3 font-medium"> Price :  </h3>
       </div>
-    </di>
+    </div> :<h2> Your Cart Is Empty <Link to="/">Go Shoping </Link></h2>
+    }
+    
     </>
   )
 }
